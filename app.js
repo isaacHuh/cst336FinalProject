bafbc8b056504bb40142081296c2c893284afc12
EC2 Default User <ec2-user@ip-172-31-31-62.us-west-2.compute.internal>
2019-11-27 00:07:20 +0000

    basic functions

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const mysql   = require("mysql");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public")); //folder for images, css, js
+
+app.get("/", async function(req, res){
+    res.render("index");
+});//root
+
+app.get("/search", async function(req, res){
+    //let rows = await getPlanets(req.query);
+    //res.render("quotes", {"records":rows});
+    res.render("search");
+});
+
+app.get("/results", async function(req, res){
+    let rows = await getPlanets(req.query);
+    //res.render("quotes", {"records":rows});
+    res.render("results", {"planets":rows});
+});
+
+function getPlanets(query){
+    
+    let keyword = query.keyword;
+    
+    let conn = dbConnection();
+    
+    return new Promise(function(resolve, reject){
+        conn.connect(function(err) {
+           if (err) throw err;
+           console.log("Connected!");
+            
+            let params = [];
+            
+           let sql = `SELECT name, price, image
+                      FROM planets
+                      WHERE 
+                      name LIKE '%${keyword}%'`;
+        
+           console.log("SQL:", sql)
+           conn.query(sql, params, function (err, rows, fields) {
+              if (err) throw err;
+              //res.send(rows);
+              resolve(rows);
+           });
+        
+        });//connect
+    });//promise
+    
+}//getQuotes
+
+//values in red must be updated
+function dbConnection(){
+
+   let conn = mysql.createConnection({
+                 host: "cst336db.space",
+                 user: "cst336_dbUser32",
+             password: "bz0olr",
+             database: "cst336_db32"
+       }); //createConnection
+
+return conn;
+
+}
+
+
+//starting server
+app.listen(process.env.PORT, process.env.IP, function(){
+console.log("Express server is running...");
+});
