be0e46e6c09bba3abb1db5480888615b3f041843
EC2 Default User <ec2-user@ip-172-31-31-62.us-west-2.compute.internal>
2019-12-11 21:49:31 +0000

    adjust

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const session = require('express-session');
 app.set("view engine", "ejs");
 app.use(express.static("public")); //folder for images, css, js
 app.use(express.urlencoded()); //use to parse data sent using the POST method
-app.use(session({ secret: 'any word', cookie: { maxAge: 60000 }}));
+app.use(session({ secret: 'any word', cookie: { maxAge: 6000000 }}));
 
 app.get("/", async function(req, res){
     res.render("index");
